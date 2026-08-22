17cc5323b318a1dddccc77df042d01dd7506eb7d
AgnetX <Abdullahesmaeal@gmail.com>
2026-08-22 21:43:03 +0200

    switch to Gemini API

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'agentx-v1';
+const FILES = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
